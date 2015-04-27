276e0431ae379eb80cd53e4a6ffe9d7f62d48c8b
Nick Alston <alston.nick@gmail.com>
2015-04-27 10:05:34 -0700

    Testing a commit in WebStorm.

--- models/dispatchServerModel.js
+++ models/dispatchServerModel.js
@@ -1,11 +1,18 @@
 var mongoose = require ('mongoose');
 var Schema = mongoose.Schema;
 
+var vehicleSchema = new Schema ({
+    NAGSId: Number,
+
+});
+
 var dispatchSchema = new Schema({
     environment: {type: String, enum: ['DEV', 'TEST', 'PROD']},
     tpaId: Number,
     branchId: Number,
     transactionLastSent: Date,
-    dateOfLoss: Date
+    dateOfLoss: Date,
+    lossId: String,
+    vehicleInfo:
     //TODO: Add more here.
 });
