a9d53c641e644981b68cfcbba944d98d0b7b6e13
Nick Alston <alston.nick@gmail.com>
2015-04-27 15:18:07 -0700

    Updated dispatch server model for DB schema

--- models/dispatchServerModel.js
+++ models/dispatchServerModel.js
@@ -1,18 +1,58 @@
-var mongoose = require ('mongoose');
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var vehicleSchema = new Schema ({
-    NAGSId: Number,
+var vehicleSchema = new Schema({
+    vehicleId: {type: Number, required: true},
+    isNAGSId: {type: Boolean, default: true, required: true},
+    year: {type: Number, min: 1900, max: 2050, required: true},
+    make: {type: String, required: true},
+    model: {type: String, required: true},
+    style: {type: String, required: true}
+});
 
+var addressSchema = new Schema({
+    description: String,
+    address: String,
+    city: String,
+    state: String,
+    province: String,
+    postalCode: String
+});
+
+var phoneSchema = new Schema({
+    phone: Number,
+    description: String,
+    isTextable: {type: Boolean, default: true}
+});
+
+var claimantSchema = new Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    addresses: [addressSchema],
+    phones: {type: [phoneSchema], required: true},
+    email: String
+});
+
+var glassVendorSchema = new Schema({
+    name: String,
+    addresses: [addressSchema],
+    phones: [phoneSchema]
 });
 
 var dispatchSchema = new Schema({
-    environment: {type: String, enum: ['DEV', 'TEST', 'PROD']},
-    tpaId: Number,
-    branchId: Number,
-    transactionLastSent: Date,
-    dateOfLoss: Date,
+    environment: {type: String, enum: ['DEV', 'TEST', 'PROD'], default: 'TEST'},
+    tpaId: {type: Number, required: true},
+    branchId: {type: Number, required: true},
+    transactionLastSent: {type: Date, required: true},
+    dateOfLoss: {type: Date, required: true},
     lossId: String,
-    vehicleInfo:
+    vehicleInfo: vehicleSchema,
+    claimantInfo: claimantSchema,
+    deductible: {type: Number, min: 0.00, required: true},
+    lossReferenceId: {type: Number, required: true},
+    glassVendor: glassVendorSchema,
+    maxRepairs: {type: Number, min: 0.00, required: true},
+    insuranceAlertMessage: {type: String, required: true},
+    VIN: {type: String, required: true}
     //TODO: Add more here.
 });
